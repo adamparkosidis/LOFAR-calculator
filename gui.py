1cd5b075518456a1251a23fc1fd4f333ba7532cf
Noa Peters <48205711+noahime@users.noreply.github.com>
2023-10-27 15:37:24 +0200

    Update gui.py
    Update the warning text to explain the different sensitivity table columns

--- gui.py
+++ gui.py
@@ -370,7 +370,11 @@ The [LOFAR Imaging Cookbook](https://support.astron.nl/LOFARImagingCookbook/calc
 This tool's theoretical rms calculation is based on [SKA Memo 113](http://www.skatelescope.org/uploaded/59513_113_Memo_Nijboer.pdf) by Nijboer, Pandey-Pommier & de Bruyn.
 It makes use of theoretical SEFD values optimised at 60 MHz. As a result, please use it with caution.
 
-\*The effective rms calculation for LBA applies a standard geometrical correction based on the target's mean elevation around its transit point for a given observation period and date.
+\*The mean elevation presented is based on the mean elevation around transit on the selected day, and is found by taking +/- half the observation time.
+
+\*\* The theoretical rms is calculated based on the instrumental set-up, and has a geometrical corrected based on the mean elevation
+
+\*\*\* The effective rms calculation for LBA is currently the same as the theoretical, which has only a standard geometrical correction.
 The effective rms calculation for HBA is based on Shimwell et al 2021's findings [The LOFAR Two-metre Sky Survey](https://doi.org/10.1051/0004-6361/202142484) from which a 5-sigma uncertainty is presented.
 
 LUCI (version 20200114) was written and is maintained for the LOFAR Science Operations & Support group by Sarrvesh Sridhar. The source code is publicly available on [GitHub](https://github.com/scisup/LOFAR-calculator).
